ed286fe40833b11dc169484ae11900f1673e84cb
Comfy Capital <165381734+tomcadene@users.noreply.github.com>
2024-04-18 21:03:45 +0200

    Update BufferGeometryUtils.js

--- three.js-master/examples/jsm/utils/BufferGeometryUtils.js
+++ three.js-master/examples/jsm/utils/BufferGeometryUtils.js
@@ -9,7 +9,7 @@ import {
 	TriangleStripDrawMode,
 	TrianglesDrawMode,
 	Vector3,
-} from 'three';
+} from '/three.js-master/build/three.module.js';
 
 function computeMikkTSpaceTangents( geometry, MikkTSpace, negateSign = true ) {
 
