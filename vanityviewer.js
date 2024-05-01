2b6cbb267b486467313dd9c818133f3f7e447c99
tomcadene <165381734+tomcadene@users.noreply.github.com>
2024-05-01 22:26:19 +0200

    Update vanityviewer.js

--- vanityviewer.js
+++ vanityviewer.js
@@ -23,6 +23,23 @@ const vvElement = document.querySelector('.vv');
 // Calculate aspect ratio of the .vv div
 const aspectRatio = vvElement.clientWidth / vvElement.clientHeight;
 
+// Create a Stats instance
+var stats = new Stats();
+var stats2 = new Stats();
+// Position it on the top-left corner
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats2.showPanel(2); // 0: fps, 1: ms, 2: mb, 3+: custom
+vvElement.appendChild(stats.dom);
+vvElement.appendChild(stats2.dom);
+// Adjust style of the stats to position it correctly within the div
+stats.dom.style.position = 'absolute';
+stats.dom.style.top = '0px';
+stats.dom.style.left = '10%';
+
+stats2.dom.style.position = 'absolute';
+stats2.dom.style.top = '0px';
+stats2.dom.style.left = '20%';
+
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
@@ -41,6 +58,9 @@ console.log('renderer.toneMappingExposure:', renderer.toneMappingExposure);
 
 vvElement.appendChild(renderer.domElement); // Append renderer to the .vv div instead of body
 
+// Apply border-radius to the canvas
+renderer.domElement.style.borderRadius = '1rem';
+
 // Resize handler to ensure the scene adjusts with the viewport/window size
 function onWindowResize() {
   // Update camera aspect ratio and renderer size when window resizes
@@ -48,7 +68,7 @@ function onWindowResize() {
   const height = vvElement.clientHeight;
   camera.aspect = width / height;
   camera.near = 0.1;
-camera.far = 100;
+  camera.far = 100;
   camera.updateProjectionMatrix();
   renderer.setSize(width, height);
   console.log('Window resized and renderer updated');
@@ -88,7 +108,7 @@ const planeMaterial = new THREE.MeshStandardMaterial({
   roughness: 1,    // Non-reflective surface
   metalness: 0,    // Non-metallic surface
   transparent: true, // Enable transparency
-  opacity: 0.85      // Adjust opacity here, 0 is fully transparent, 1 is fully opaque
+  opacity: 1      // Adjust opacity here, 0 is fully transparent, 1 is fully opaque
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = - Math.PI / 2; // Rotate to lay flat
@@ -146,12 +166,12 @@ const toggleRenderingButton = document.getElementById('dofButton');
 let useComposer = false;
 
 toggleRenderingButton.addEventListener('click', function () {
-    useComposer = !useComposer;
-    if(useComposer){
-        console.log('Switched to composer rendering');
-    } else {
-        console.log('Switched to standard rendering');
-    }
+  useComposer = !useComposer;
+  if (useComposer) {
+    console.log('Switched to composer rendering');
+  } else {
+    console.log('Switched to standard rendering');
+  }
 });
 
 // Calculate the Euclidean distance between the camera and the cube at the origin
@@ -214,6 +234,8 @@ console.log('Clear Alpha:', renderer.getClearAlpha());
 
 // Animation loop
 function animate() {
+  stats.begin(); // Start measuring
+  stats2.begin(); // Start measuring
   requestAnimationFrame(animate);
 
   // cube.rotation.x += 0.01;
@@ -224,15 +246,28 @@ function animate() {
   if (useComposer) {
     renderer.toneMappingExposure = 1.25; // Make sure this matches the initial setting
     composer.render(); // Use post-processing rendering
-} else {
+  } else {
     renderer.render(scene, camera); // Use standard rendering
-}
+  }
+  stats.end(); // Stop measuring
+  stats2.end(); // Stop measuring
 }
 
 animate();
 
-// Button functionality to change the opacity
-document.getElementById('opacityButton').addEventListener('click', function() {
-  cube.material.opacity = cube.material.opacity === 1 ? 0.5 : 1; // Toggle between 0.5 and 0.85
-  console.log('Opacity changed to: ' + cube.material.opacity); // Log the change for verification
+// animation
+renderer.setAnimationLoop(animation);
+function animation(time) {
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+}
+
+document.getElementById('cubeOpacityButton').addEventListener('click', function () {
+  cube.material.opacity = cube.material.opacity === 1 ? 0 : 1; // if cube opacity === 1 change to 0 else 1
+  console.log('Cube opacity changed to: ' + cube.material.opacity); // Log the change for verification
+});
+
+document.getElementById('planeOpacityButton').addEventListener('click', function () {
+  plane.material.opacity = plane.material.opacity === 1 ? 0 : 1; // if plane opacity === 1 change to 0 else 1
+  console.log('Plane opacity changed to: ' + plane.material.opacity); // Log the change for verification
 });
