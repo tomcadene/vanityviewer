390d58aed9aa46271c0aa6ba54e841e18bdce3b6
tomcadene <165381734+tomcadene@users.noreply.github.com>
2024-05-15 22:02:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const vvElement = document.querySelector('.vv');
 
+const MODEL_PATH = '/models/brass_goblets_2k.gltf/brass_goblets_2k.gltf'; // Path to the model file
 
 const USE_BACKGROUND_TEXTURE = true; // Set this to true to enable background texture
 const USE_SUN_LIGHT = true; // Set this to true to add a plane to the scene
 const ADD_PLANE_TO_THE_SCENE = false; // Set this to true to add a plane to the scene
 
-export { vvElement, USE_BACKGROUND_TEXTURE, USE_SUN_LIGHT, ADD_PLANE_TO_THE_SCENE };
+export { vvElement, MODEL_PATH, USE_BACKGROUND_TEXTURE, USE_SUN_LIGHT, ADD_PLANE_TO_THE_SCENE };
