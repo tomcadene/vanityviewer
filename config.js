d3bf3ee7b13d3b8897b2b2aa07f01e58c71a4fb7
tomcadene <165381734+tomcadene@users.noreply.github.com>
2024-05-15 11:17:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const USE_BACKGROUND_TEXTURE = true; // Set this to true to enable background texture
 const USE_SUN_LIGHT = true; // Set this to true to add a plane to the scene
 const ADD_PLANE_TO_THE_SCENE = false; // Set this to true to add a plane to the scene
+
+export { USE_BACKGROUND_TEXTURE, USE_SUN_LIGHT, ADD_PLANE_TO_THE_SCENE };
