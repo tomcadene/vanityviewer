ef4539769fd91a1e123717ac4df67bb994e2d7d5
tomcadene <165381734+tomcadene@users.noreply.github.com>
2024-05-16 22:19:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 const USE_BACKGROUND_TEXTURE = true; // Set this to true to enable background texture
-const USE_SUN_LIGHT = true; // Set this to true to add a plane to the scene
+const USE_SUN_LIGHT = true; // Set this to true to add a sun light to the scene
 const ADD_PLANE_TO_THE_SCENE = false; // Set this to true to add a plane to the scene
+
+// PERFORMANCE_MONITOR
+const ADD_PERFORMANCE_MONITOR = false; // Set this to true to add a performance monitor to the scene
+// Notes
+// FPS Frames rendered in the last second. The higher the number the better.
+// MS Milliseconds needed to render a frame. The lower the number the better.
+// MB MBytes of allocated memory.
+// Source: https://github.com/mrdoob/stats.js
 
 export { USE_BACKGROUND_TEXTURE, USE_SUN_LIGHT, ADD_PLANE_TO_THE_SCENE };
